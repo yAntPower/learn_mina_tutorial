c80f1945b8e0c3b8b4c800f98a1af6bd474f79a6
evanshapiro8 <evanshapiro8@outlook.com>
2025-06-27 23:19:52 +0800

    add new logic

--- 01-hello-world/src/main.ts
+++ 01-hello-world/src/main.ts
@@ -32,3 +32,15 @@ await txn1.prove();
 await txn1.sign([senderKey]).send();
 const num1 = zkAppInstance.num.get();
 console.log('state after txn1:',num1.toString());
+
+try {
+    const txn2 = await Mina.transaction(senderAccount,async()=>{
+        await zkAppInstance.update(Field(81));
+    });
+    await txn2.prove();
+    await txn2.sign([senderKey]).send();
+}catch (error: any) {
+    console.error('Error during transaction 2:', error.message);
+}
+const num2 = zkAppInstance.num.get();
+console.log('state after txn2:',num2.toString());   
